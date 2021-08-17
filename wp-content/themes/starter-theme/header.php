44a2be88df45c2c45f6717b53486ef351189fb6c
Marcelo Henrique <marcelohphn@gmail.com>
2021-08-17 19:51:07 -0300

    Correções logo

--- wp-content/themes/starter-theme/header.php
+++ wp-content/themes/starter-theme/header.php
@@ -21,21 +21,26 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand" href="<?php echo home_url(); ?>">
-            <?php bloginfo('name'); ?>
-        </a>
-        <?php
-        wp_nav_menu( array(
-            'theme_location' => 'menu-navegacao',
-            'depth' => 2,
-            'container' => 'div',
-            'container_class' => 'collapse navbar-collapse',
-            'container_id' => 'bs-example-navbar-collapse-1',
-            'menu_class' => 'nav navbar-nav',
-            'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-            'walker' => new WP_Bootstrap_Navwalker())
-        );
-        ?>
+        <div class="col-md-2">
+            <a class="navbar-brand" href="<?php echo home_url(); ?>">
+                <?php the_custom_logo(); ?>
+            </a>
+        </div>
+        <div class="col">
+        
+            <?php
+            wp_nav_menu( array(
+                'theme_location' => 'menu-navegacao',
+                'depth' => 2,
+                'container' => 'div',
+                'container_class' => 'collapse navbar-collapse',
+                'container_id' => 'bs-example-navbar-collapse-1',
+                'menu_class' => 'nav navbar-nav',
+                'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+                'walker' => new WP_Bootstrap_Navwalker())
+            );
+            ?>
+        </div>
     </div>
 </nav>
         
